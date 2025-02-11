54eadab50d05a58d4009b8e99d3918c7b4d66614
changukang <changukang@korea.ac.kr>
2025-02-12 01:11:46 +0900

    update

--- state_preparation/state_prep_algorithms.py
+++ state_preparation/state_prep_algorithms.py
@@ -1,33 +1,36 @@
 import logging
 import time
 from dataclasses import dataclass
+import qiskit
 
 import cirq
 import numpy as np
 import xyz
+from qclib.state_preparation import LowRankInitialize
 from qiskit import transpile
 from qiskit_aer import AerSimulator
-
-from qclib.state_preparation import LowRankInitialize
 from sp.qiskit_to_cirq import qiskit2cirq
 from sp.special_states import GenearlizdeWTypeState, GenearlizdeWTypeStateWithPLU
 from sp.special_three_qubit_states import ThreeQubitWType
 from sp.symbolic_expression.w_type import *
 from sp.utils import get_number_of_cnot
+from functools import cached_property
 
 logger = logging.getLogger(__name__)
 
 
 class StatePreparation:
 
-    def run(self, state_vector: np.ndarray) -> "StatePreparationResult": ...
+    def run(
+        self, state_vector: np.ndarray, target_object: str = "cirq"
+    ) -> "StatePreparationResult": ...
 
 
 class QuantumXYZ(StatePreparation):
+    # implementation of https://arxiv.org/abs/2401.01009
 
     def run(self, state_vector: np.ndarray, target_object: str = "cirq"):
-        logger.info("State to Prepare")
-        logger.info(cirq.dirac_notation(state_vector))
+        logger.info(f"State to Prepare : {cirq.dirac_notation(state_vector)}")
         logger.info(f"num qubit : {int(np.log2(state_vector.shape[0]))}")
         logger.info("Running XYZ")
         target_state = xyz.quantize_state(state_vector)
@@ -43,6 +46,23 @@ class QuantumXYZ(StatePreparation):
         return t_circuit, (end_time - start_time)
 
 
+class LowRankStatePrep(StatePreparation):
+    # implementation of https://arxiv.org/abs/2111.03132
+
+    def run(self, state_vector: np.ndarray, target_object: str = "cirq"):
+        logger.info(f"State to Prepare : {cirq.dirac_notation(state_vector)}")
+        logger.info(f"num qubit : {int(np.log2(state_vector.shape[0]))}")
+        logger.info("Running LowRankStatePrep")
+        backend = AerSimulator()
+        circuit = LowRankInitialize(state_vector).definition
+        transpiled_circuit = transpile(
+            circuit, backend, basis_gates=["u3", "cx"], optimization_level=3
+        )
+        return transpiled_circuit
+
+class IsometryBased(StatePreparation):
+    pass
+
 @dataclass
 class StatePreparationResult:
 
@@ -50,11 +70,34 @@ class StatePreparationResult:
         self,
         state_prep_engine: StatePreparation,
         goal_sv: np.ndarray,
-        circuit: cirq.Circuit,
+        circuit: Union[cirq.Circuit, qiskit.QuantumCircuit],
+        elapsed_time : float
     ):
         self.elapsed_time = None
         self.num_cnot = None
         self.cnot_depth = None
+        self.circuit = None
 
     def export(self):
         raise NotImplementedError
+
+    @staticmethod
+    def validate_circuit(circuit):
+        pass
+
+    @cached_property
+    def cirq_circuit(self):
+        pass
+
+    @cached_property
+    def qiskit_circuit(self):
+        pass
+
+    @cached_property
+    def num_cnot(self):
+        pass
+
+    @cached_property
+    def depth(self):
+        pass
+    
--- state_preparation/utils.py
+++ state_preparation/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cirq, qiskit
 
 def num_qubit(state_vector: np.ndarray) -> int:
     log2_res = np.log2(state_vector.shape[0])
@@ -7,3 +7,10 @@ def num_qubit(state_vector: np.ndarray) -> int:
     if in_int != int(log2_res):
         raise ValueError(f"Invalid Quantum State Vector : {state_vector}")
     return in_int
+
+
+def validate_result_cirq_circuit(circuit : cirq.Circuit):
+    pass
+
+def validate_result_qiskit_circuit(circuit : cirq.Circuit):
+    pass

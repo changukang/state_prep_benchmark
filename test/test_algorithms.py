a1736e7281da48772bde1032072c0349c50430a7
changukang <changukang@korea.ac.kr>
2025-03-30 16:34:59 +0900

    fix tc

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -1,18 +1,23 @@
 from state_preparation.algorithms import IsometryBased, LowRankStatePrep
 from state_preparation.state_samplers import get_random_state
-
+from state_preparation.results import StatePreparationResult
+import cirq
+import numpy as np
 
 def test_low_rank():
     for seed in range(10):
         sv = get_random_state(num_qubit=5, seed=seed)
         prepare = LowRankStatePrep()
-        state_prep_res = prepare.run(sv)
-        print(state_prep_res)
-
+        state_prep_res : StatePreparationResult = prepare.run(sv)
+        qc = state_prep_res.cirq_circuit
+        sv_from_result = cirq.final_state_vector(qc, dtype=np.complex128)
+        assert cirq.equal_up_to_global_phase(sv, sv_from_result)
 
 def test_isometry():
     for seed in range(10):
         sv = get_random_state(num_qubit=5, seed=seed)
         prepare = IsometryBased()
         state_prep_res = prepare.run(sv)
-        print(state_prep_res)
+        qc = state_prep_res.cirq_circuit
+        sv_from_result = cirq.final_state_vector(qc, dtype=np.complex128)
+        assert cirq.equal_up_to_global_phase(sv, sv_from_result)
